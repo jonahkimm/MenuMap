2a2defc3252732f4e3f0eb088dff32733a86448e
AarhamH <aarham.haider@gmail.com>
2023-11-20 21:08:21 -0800

    Testing math

---
+++ src/math.test.js
@@ -0,0 +1,17 @@
+import sum from './math'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + 1 to equal 0', () => {
+  expect(sum(-1, 1)).toBe(0);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('adds -1 + -2 to equal -3', () => {
+  expect(sum(-1, -2)).toBe(-3);
+});
