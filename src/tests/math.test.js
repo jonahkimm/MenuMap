1faf3a8c2488cd2025af5a082273668fac9b0369
AarhamH <aarham.haider@gmail.com>
2023-11-20 20:45:45 -0800

    Add math test; should pass

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -1,7 +1,7 @@
 import sum from "./math"
 
 describe("test sum", () => {
-    test("adds 2+2 to equal 4", () => {
-        expect(sum(2,2)).toBe(4);
+    test("adds 2+3 to equal 5", () => {
+        expect(sum(3,2)).toBe(5);
     });
 })
