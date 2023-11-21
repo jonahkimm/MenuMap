a499a41be8cca3c5f927f1c8751c79aea9f68e3c
jeetpatel1113 <jmpatel7238@gmail.com>
2023-11-20 19:09:37 -0800

    Added Google Maps in MenuMap :D

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Sidebar from './components/Sidebar.jsx';
 import MiddleSearch from './components/MiddleSearch.jsx';
 import Header from './components/Header.jsx'
+import EmbededMap from './components/EmbededMap.jsx';
 
 function App() {
-    return(
-        <div className= "App">
-            <Header/>
-            <Sidebar/>
-            <MiddleSearch/>
-        </div>
+  return (
+    <div className="App">
+      <Header />
+      <Sidebar />
+      <MiddleSearch />
+      <EmbededMap />
+    </div>
 
-    );
+  );
 }
 
 export default App;
