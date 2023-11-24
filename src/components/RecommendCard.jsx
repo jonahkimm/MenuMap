5e96c3c987962b125feebf96dc7dc4562bb8d771
AarhamH <aarham.haider@gmail.com>
2023-11-23 19:57:10 -0800

    Added recommendations popup and modified style

--- src/components/RecommendCard.jsx
+++ src/components/RecommendCard.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import RecommendPopup from './RecommendPopup';
 var foodImage = require('../assets/testpic.jpg');
 
 // Rescard is a class-based component that will pull class objects from API query
 export default class RecommendCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      openResCard: false
+    };
+  }
   render() {
-    const { name, author, image } = this.props;
+    const { name, author, image, nutrition, description } = this.props;
 
     return (
-      <div className='font-sans h-40 p-5 relative top-1 bg-gray-100 mb-2 border-4 border-gray-200 hover:border-pink-400'>
+      <div className='font-sans h-40 p-5 relative top-1 bg-gray-100 mb-2 ' >
+        <div data-testid='rescard' className='absolute rounded-lg left-0 top-0 p-5 h-40 w-full bg-rose-200 border-4 border-gray-200 hover:border-pink-400' onClick={() => this.setState({ openResCard: true })} >
+
+
         <div id="resname">
-          <h1 className='text-xl font-bold absolute text-blue left-32'>{name}</h1>
+          <h1 className='text-xl top-2 font-semibold leading-snug absolute text-blue left-36'>{name}</h1>
         </div>
 
         <div id="foodpic">
-          <img src={image} className='object-scale-down h-3/4 w-2/5 absolute top-2 left-0 pr-7'  alt={foodImage} />
+          <img src={image} className='object-fill h-3/4 w-2/5 absolute top-2 ml-2 left-0 pr-7 rounded-lg'  alt={foodImage} />
         </div>
 
-        <div id="review" className="absolute left-32 top-20 flex">
-          <h1 className="font-bold">Author: </h1>
+        <div id="review" className="absolute left-36 top-24 flex">
+          <h1 className="font-semibold leading-snug">Author: </h1>
           <div>
-            <p>{author}</p>
+          <p className='font-light leading-snug'> {author !== null ? author : 'n/a'}</p>
           </div>
         </div>
+        </div>
+        createPortal (
+        <RecommendPopup restaurant={{ name: name, author:author, image:image, nutrition:nutrition, description:description}} open={this.state.openResCard} onClose={() => this.setState({ openResCard: false })} />
+        )
       </div>
     );
   }
