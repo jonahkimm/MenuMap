1e5d26bd592894014089466652770f35d3b3cb32
Jonah Kim <jonahk@sfu.ca>
2023-11-25 13:26:37 -0800

    Took out sidebar call from app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header.jsx'
 import EmbededMap from './components/EmbededMap.jsx';
 import Container from './components/Container.jsx';
-import Sidebar from './components/Sidebar.jsx';
+
 
 function App() {
 
