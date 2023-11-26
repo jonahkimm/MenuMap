d553b8293f93487e5b1366c760f2dad6891e310a
AarhamH <aarham.haider@gmail.com>
2023-11-26 02:23:46 -0800

    Api key issue fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,7 +8,7 @@ export default function Intro()
 {
     const position = {lat:54.32,lng:10}
     return (
-        <APIProvider apiKey="AIzaSyAIi0GfFZUi95zJ-QQtMe_RY1nZgEcUDhc">
+        <APIProvider apiKey="////AIzaSyAIi0GfFZUi95zJ-QQtMe_RY1nZgEcUDhc">
             <div style={{height:"100vh"}}>
                 <Map zoom={9} center={position}></Map>  
             </div>
