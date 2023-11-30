dd030e9349f320c8e03385e49b2ca0200d069cd3
SilianRail <aryamancodesc@gmail.com>
2023-11-29 20:05:25 -0800

    Popup Map Window

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,49 +1,44 @@
-"use-client";
-
 import { useState } from "react";
 import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow,
+    APIProvider,
+    Map,
+    AdvancedMarker,
+    Pin,
+    InfoWindow,
 } from "@vis.gl/react-google-maps";
-import useLocationFetch from "../hooks/useLocationFetch";
+import useLocationFetch from '../hooks/useLocationFetch';
 
-export default function Intro() {
-  const location = useLocationFetch();
-  const userLatitude = Number(location.userLocation.coordinates.latitude);
-  const userLongitude = Number(location.userLocation.coordinates.longitude);
+export default function EmbededMap() {
+    const location = useLocationFetch();
+    const userLatitude = Number(location.userLocation.coordinates.latitude);
+    const userLongitude = Number(location.userLocation.coordinates.longitude);
 
-  const style = {
-    height: "88.5vh",
-    width: "33%",
-    right: 0,
-    top: "11.5vh",
-    position: "absolute",
-    zIndex: -10,
-  };
+    const position = {lat:userLatitude,lng:userLongitude}
+    
+    const[open, setOpen] = useState(false);
 
-  const position = { lat: userLatitude, lng: userLongitude };
-  const { open, setOpen } = useState(false);
-  return (
-    <APIProvider apiKey="////AIzaSyAIi0GfFZUi95zJ-QQtMe_RY1nZgEcUDhc">
-      <div>
-        <Map zoom={15} center={position} style={style} mapId="30c45397d92e6144">
-          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-            <Pin
-              background={"red"}
-              borderColor={"black"}
-              glyphColor={"yellow"}
-            />
-          </AdvancedMarker>
-          {open && (
-            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-              <p>Current location</p>
-            </InfoWindow>
-          )}
-        </Map>
-      </div>
+        const style = {
+        height: '100%',
+        width: '33%',
+        right: 0,
+        top: '11.5vh',
+        position: 'absolute',
+    }
+
+    return (
+    <APIProvider apiKey="AIzaSyAwR7DLNCBpRs-vdVSzECotAGvKKXx745k"> 
+
+        <div style={{height: "100vh", width: "100%"}}> 
+            <Map zoom={9} center={position} mapId="30c45397d92e6144" style={style} >
+                <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+                    <Pin background={"pink"} borderColor={"black"} glyphColor={"red"}/>
+                </AdvancedMarker>
+
+                {open && <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                    <p>some random information</p>
+                </InfoWindow>}
+            </Map>
+        </div>
     </APIProvider>
-  );
+    );
 }
