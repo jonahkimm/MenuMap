2f34a5153c6c88234950d08bd4709dc70af286ff
AarhamH <aarham.haider@gmail.com>
2023-11-30 16:36:34 -0800

    Integrated grids for dynamic resizing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Container from './components/Container.jsx';
-import Map from './components/Map.jsx'
 
 function App() {
 
